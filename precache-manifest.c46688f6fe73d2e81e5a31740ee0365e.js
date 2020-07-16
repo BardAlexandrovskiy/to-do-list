f5af28b77ef738377d4f58360209fcfb8af453e9
paranoidb <alexandr8558@gmail.com>
2020-07-16 03:29:43 +0300

    Updates

---
+++ precache-manifest.c46688f6fe73d2e81e5a31740ee0365e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d59fff785b463f8dc29885592efcdf",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "a399d78e93a0df57c768",
+    "url": "/to-do-list/static/js/2.3f944b89.chunk.js"
+  },
+  {
+    "revision": "bb8fb1c4ca51bfe3d3f76c8caf6f2c92",
+    "url": "/to-do-list/static/js/2.3f944b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa29c0a37f632468a1d",
+    "url": "/to-do-list/static/js/main.fe5ddb0f.chunk.js"
+  },
+  {
+    "revision": "99620f4632993b275b31",
+    "url": "/to-do-list/static/js/runtime-main.65d94b1a.js"
+  }
+]);
